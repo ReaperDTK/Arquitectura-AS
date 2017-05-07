f39ca201adc2e77d8bbe45e9741e820066096803
Jose Antonio Dosil Toja <jose.dosilt@udc.es>
2017-05-07 12:25:35 +0200

    Cambiado la forma de parar el servidor
    Antes cualquier proceso, incluido los remotos podían para el servidor.
    Ahora se controla que solo el proceso que lo ha iniciado pueda pararlo.

--- src/main/server.erl
+++ src/main/server.erl
@@ -18,24 +18,35 @@ start()->
         _ -> ok
     end,
     erlang:set_cookie(node(),chat),
-    Pid = spawn(fun() -> listen_loop([]) end),
+    Control=self(),
+    Pid = spawn(fun() ->   init_srv(Control) end),
     %% Registra el pid como server para este shell de erlang
     register(server,Pid),
     %% Registra el pid como server globalmente, pàra todos los nodos
     global:register_name(server,Pid).
 
 stop() ->
-    server ! stop.
+    exit(whereis(server),stop).
 
-listen_loop(U)->
+
+init_srv(Control) ->
+    %% Hace que cuando mandan un exit(PID,Reason) en lugar de hacer saltar una excepcion
+    % el proceso reciba un mensaje {'EXIT',From,Reason}
+    process_flag(trap_exit,true),
+    listen_loop([],Control).
+
+listen_loop(U,Control)->
     receive
         %% LLega una peticion de conexión al servidor
-        {con,A} -> listen_loop([A|U]);
+        {con,A} -> listen_loop([A|U],Control);
         %% Desconexión del servidor
-        {disc,A} -> listen_loop([X || X <- U, X/=A]);
+        {disc,A} -> listen_loop([X || X <- U, X/=A],Control);
         %%Mensaje
-        {msg,M,From} ->  send_msg({msg,M,From},From,U), listen_loop(U);
-        stop ->global:unregister_name(server) ,ok
+        {msg,M,From} ->  send_msg({msg,M,From},From,U), listen_loop(U,Control);
+        %% Controla que el unico proceso que puede cerrar el servidor es el que lo ha creado
+        {'EXIT',Control,stop} ->global:unregister_name(server) ,ok;
+        %% Ignora el resto de mensajes
+        _ -> listen_loop(U,Control)
     end.
 
 
